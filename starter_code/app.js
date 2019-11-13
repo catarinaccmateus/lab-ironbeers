a5fc2712f74bc477c39c41092857876ba5575a74
catarinaccmateus <catarina_1102@hotmail.com>
2019-11-13 18:08:06 +0000

    needs more css but main objectives completed

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -11,11 +10,54 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static("public"));
+
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
 
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers', {
+        beers: beers
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/random-beers', (req, res, next) => {
+
+  punkAPI.getRandom()
+    .then(beers => {
+      res.render('random-beers', {
+        beer: beers[0]
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+
+});
+
+app.get('/random-beers/:id', (req, res, next) => {
+  console.log(req.params.id);
+
+  punkAPI.getBeer(req.params.id)
+  .then(beers => {
+    res.send(beers)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
+
+//GETTING AN ID FROM THE URL AND DISPLAY A BEER
